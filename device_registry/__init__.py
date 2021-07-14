4e66c9c511a14b847afc9f1e1a2b3b0eb3f8b9f7
Windows10 <abhi.anu003@gmail.com>
2021-07-14 16:46:24 +0530

    HTML page of ReadMe.md :ez:

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,3 +1,14 @@
 from flask import Flask
-
+import os
+import markdown
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Some Documentation"""
+
+    # Open the ReadMe File
+    with open(os.path.dirname(app.root_path) + '/ReadMe.md', 'r' ) as markdown_file:
+        content = markdown_file.read()
+
+        return markdown.markdown(content)
